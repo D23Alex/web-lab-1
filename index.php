96b3a1ef8df2a2820536b63d51fef0aa80118572
D23Alex <aleksejdormorezov@gmail.com>
2022-09-01 19:16:23 +0000

    simplified forms added to index.php for testing

--- index.php
+++ index.php
@@ -12,8 +12,30 @@
 </head>
 
 <body>
-
+<p><?php
+    if (isset($_GET["x-input"]))
+        echo $_GET["x-input"] . "<br>";
+    if (isset($_GET["y-input"]))
+        echo $_GET["y-input"] . "<br>";
+    if (isset($_GET["r-input"]))
+        echo $_GET["r-input"] . "<br>";
+    ?></p>
 <p>Hello, world! [from index.php]</p>
+<form method="get">
+    <p>X</p>
+    <input type="radio" id="x-equals-0.5-radio" name="x-input" value="0,5">
+    <label for="x-equals-0.5-input">0,5</label><br>
+    <input type="radio" id="x-equals-1-radio" name="x-input" value="1">
+    <label for="x-equals-1-input">1</label>
+    <p>Y</p>
+    <input type="text" id="y-text-input" name="y-input">
+    <label for="y-text-input">label-for-Y</label>
+    <p>R</p>
+    <input type="button" id="r-equals-0.5-radio" name="r-input" value="0,5">
+    <label for="r-equals-0.5-input">0,5</label><br>
+    <input type="button" id="r-equals-1-radio" name="r-input" value="1">
+    <label for="r-equals-1-input">1</label>
+</form>
 
 <script src="js/script.js"></script>
 </body>
