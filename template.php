9647d7db72cebd284a37523e0a51b55d62bb2e4d
D23Alex <aleksejdormorezov@gmail.com>
2022-09-04 11:52:29 +0000

    add layout

--- template.php
+++ template.php
@@ -12,7 +12,25 @@
 </head>
 
 <body>
-<p>Hello, world! [from template.php]</p>
+<div class="wrapper">
+    <header>
+        <div class="header-content">
+            HEADER SOME TEXT
+        </div>
+    </header>
+
+    <main>
+        <div class="main-content">
+            MAIN SOME TEXT
+        </div>
+    </main>
+
+    <footer>
+        <div class="footer-content">
+            FOOTER SOME TEXT
+        </div>
+    </footer>
+</div>
 
 <script src="js/script.js"></script>
 </body>
