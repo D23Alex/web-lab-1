c28b4d21cd853faac6546ede9686ac11b7cf6048
D23Alex <aleksejdormorezov@gmail.com>
2022-09-19 08:47:27 +0300

    add style to pressed r-button

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,11 +1,18 @@
-function buttonPressed(e) {
-    let realRField = document.getElementById('real-r');
-    realRField.value = e.target.value;
-}
-
-
 const rButtons = document.getElementsByClassName("r-button");
 
+
+function rButtonPressed(e) {
+    // read value from the pressed button
+    let realRField = document.getElementById('real-r');
+    realRField.value = e.target.value;
+
+    // apply button style "selected" to selected button, remove this style from other buttons
+    for (let currentRButton of rButtons) {
+        currentRButton.classList.remove("r-button-pressed")
+    }
+    e.target.classList.add("r-button-pressed")
+}
+
 for (let currentRButton of rButtons) {
-    currentRButton.addEventListener('click', buttonPressed)
+    currentRButton.addEventListener('click', rButtonPressed)
 }
