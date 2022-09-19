4febe1f0974c3387112a08a3e7aa0057bdbf6f0e
D23Alex <aleksejdormorezov@gmail.com>
2022-09-19 10:30:33 +0300

    implement validation

--- __main-form.inc.php
+++ __main-form.inc.php
@@ -1,19 +1,44 @@
-<form name="main-form" method="get" action="/">
-    <div class="user-input" id="x-input"></div>
-    <p>X</p>
-    <input type="radio" id="x-equals-0.5-radio" name="x-input" value="0.5">
-    <label for="x-equals-0.5-input">0,5</label><br>
-    <input type="radio" id="x-equals-1-radio" name="x-input" value="1">
-    <label for="x-equals-1-input">1</label>
-    <p>Y</p>
-    <input type="text" id="y-text-input" name="y-input">
-    <label for="y-text-input">label-for-Y</label>
-    <p>R</p>
-    <input class="r-button" type="button" id="r-equals-0.5" value="0.5">
-    <label for="r-equals-0.5-input">0,5</label><br>
-    <input class="r-button" type="button" id="r-equals-1" value="1">
-    <label for="r-equals-1-input">1</label>
-    <input type="text" id="real-r" name="r-input" style="display: none">
+<form name="main-form" method="get" onsubmit="validateMainForm()" action="/">
+    <div class="user-input" id="x-input">
+        <div class="inputs">
+            <p>X</p>
+            <input type="radio" id="x-equals-0.5-radio" name="x-input" value="0.5">
+            <label for="x-equals-0.5-input">0,5</label><br>
+            <input type="radio" id="x-equals-1-radio" name="x-input" value="1">
+            <label for="x-equals-1-input">1</label>
+        </div>
+        <div class="validation-result" id="x-validation-result">
+
+        </div>
+
+    </div>
+
+    <div class="user-input" id="y-input">
+        <div class="inputs">
+            <p>Y</p>
+            <input type="text" id="y-text-input" name="y-input">
+            <label for="y-text-input">label-for-Y</label>
+        </div>
+        <div class="validation-result" id="y-validation-result">
+
+        </div>
+
+    </div>
+
+    <div class="user-input" id="r-input">
+        <div class="inputs">
+            <p>R</p>
+            <input class="r-button" type="button" id="r-equals-0.5" value="0.5">
+            <label for="r-equals-0.5-input">0,5</label><br>
+            <input class="r-button" type="button" id="r-equals-1" value="1">
+            <label for="r-equals-1-input">1</label>
+            <input type="text" id="real-r" name="r-input" style="display: none">
+        </div>
+        <div class="validation-result" id="r-validation-result">
+
+        </div>
+
+    </div>
     <button type="submit">SUBMIT BUTTON</button>
 
 
--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,11 @@
 class ValidationResult {
     _isValid;
+    _validationMessage;
+
+    constructor(isValid, issueMessage) {
+        this._isValid = isValid;
+        this._validationMessage = issueMessage;
+    }
 
     get isValid() {
         return this._isValid;
@@ -9,12 +15,12 @@ class ValidationResult {
         this._isValid = value;
     }
 
-    get issue() {
-        return this._issue;
+    get validationMessage() {
+        return this._validationMessage;
     }
 
-    set issue(value) {
-        this._issue = value;
+    set validationMessage(value) {
+        this._validationMessage = value;
     }
 }
 
@@ -22,16 +28,75 @@ function validateMainForm() {
     let xValidationResult = validateXInput();
     let yValidationResult = validateYInput();
     let rValidationResult = validateRInput();
+    constructValidationDivs(xValidationResult, yValidationResult, rValidationResult);
+    if (xValidationResult._isValid == false || yValidationResult._isValid == false || rValidationResult._isValid == false) {
+        return false
+    }
 }
 
 function validateXInput() {
     var x = document.forms["main-form"]["x-input"].value;
+    if (x == null) {
+        return new ValidationResult(false, "fill in the value of x")
+    }
+    return new ValidationResult(true, "OK")
 }
 
 function validateYInput() {
+    var minYValue = -3;
+    var maxYValue = 3;
 
+    var yInput = document.forms["main-form"]["y-input"];
+    if (yInput.value == null) {
+        return new ValidationResult(false, "fill in the value of y")
+    }
+
+    if (yInput.value.charAt(0) == ".") {
+        return new ValidationResult(false, "this field cannot start with a period");
+    }
+
+    var acceptableValues = [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for (var i = 0; i < yInput.value; i++) {
+        if (!acceptableValues.includes(yInput.value.charAt(i))) {
+            return new ValidationResult(false, "this field must be a an integer or a float, '.' separator must be used");
+        }
+    }
+
+    if (minYValue > parseFloat(yInput.value) || maxYValue < parseFloat(yInput.value)) {
+        return new ValidationResult(false, "the value of y is out of range");
+    }
+
+    return new ValidationResult(true, "OK");
 }
 
 function validateRInput() {
+    var r = document.forms["main-form"]["r-input"].value;
+    if (r == null) {
+        return new ValidationResult(false, "fill in the value of r")
+    }
+    return new ValidationResult(true, "OK")
+}
 
+function constructValidationDivs(xValidationResult, yValidationResult, rValidationResult) {
+    let xValidationDiv = document.getElementById('x-validation-result');
+    let yValidationDiv = document.getElementById('y-validation-result');
+    let rValidationDiv = document.getElementById('r-validation-result');
+
+    if (xValidationResult._isValid) {
+        xValidationDiv.classList.remove("invalid")
+        xValidationDiv.classList.add("valid")
+    }
+    xValidationDiv.title = xValidationResult._validationMessage;
+
+    if (yValidationResult._isValid) {
+        yValidationDiv.classList.remove("invalid")
+        yValidationDiv.classList.add("valid")
+    }
+    yValidationDiv.title = yValidationResult._validationMessage;
+
+    if (rValidationResult._isValid) {
+        rValidationDiv.classList.remove("invalid")
+        rValidationDiv.classList.add("valid")
+    }
+    rValidationDiv.title = rValidationResult._validationMessage;
 }
