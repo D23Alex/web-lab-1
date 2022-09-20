198e76e1dc2797553e8806e8f7b3478cedea9869
D23Alex <aleksejdormorezov@gmail.com>
2022-09-20 09:21:25 +0300

    pimp my css

--- __footer.inc.php
+++ __footer.inc.php
@@ -1,6 +1,7 @@
 <footer>
     <div class="footer-content">
-        <div class="footer-line">No copyright. Free software. Source code available on <a href="www.github.com/d23alex/web-lab-1">GitHub. Aleksei Dormorezov, web programming lab 1.</a></div>
+        <div class="footer-line">Aleksei Dormorezov, web programming lab 1.</div>
+        <div class="footer-line">No copyright. Free software. Source code available on <a href="www.github.com/d23alex/web-lab-1">GitHub.</a></div>
     </div>
 </footer>
 </div>
